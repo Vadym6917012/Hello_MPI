dd49c2929f7ef2305c7f8d60a29b0e2235359105
Vadym <vadimr69170@gmail.com>
2024-03-08 15:36:31 +0200

    Add project files.

---
+++ Exp2.cpp
@@ -0,0 +1,29 @@
+//#include "mpi.h"
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main(int argc, char* argv[])
+//{
+//    int myrank, numprocs, count;
+//    char message[20];
+//    MPI_Status status;
+//    int TAG = 0;
+//
+//    MPI_Init(&argc, &argv);
+//    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+//
+//    if (myrank == 0)
+//    {
+//        strcpy(message, "Hi, Second Processor!");
+//        MPI_Send(message, strlen(message) + 1, MPI_CHAR, 1, TAG, MPI_COMM_WORLD);
+//    }
+//    else 
+//    {
+//        MPI_Recv(message, 20, MPI_CHAR, 0, TAG, MPI_COMM_WORLD, &status);
+//        printf("received: %s\n", message);
+//    }
+//
+//    MPI_Finalize();
+//    return 0;
+//}
