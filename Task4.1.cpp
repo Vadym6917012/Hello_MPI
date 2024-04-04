760cad093e6b976b81745cae4dc4b9c56f37796b
Vadym <vadimr69170@gmail.com>
2024-04-04 15:17:08 +0300

    Lab 5. Done

--- Task4.1.cpp
+++ Task4.1.cpp
@@ -1,68 +1,68 @@
-﻿#include <mpi.h>
-#include <stdio.h>
-
-struct newtype
-{
-    float a;
-    float b; 
-    int n;
-};
-
-int main(int argc, char* argv[])
-{
-    int myrank;
-    MPI_Datatype NEW_MESSAGE_TYPE;
-    int block_lengths[3];
-    MPI_Aint displacements[3];
-    MPI_Aint addresses[4];
-    MPI_Datatype typelist[3];
-    int blocks_number;
-    struct newtype indata;
-    int tag = 0;
-    MPI_Status status;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
-    typelist[0] = MPI_FLOAT;
-    typelist[1] = MPI_FLOAT;
-    typelist[2] = MPI_INT;
-
-    block_lengths[0] = block_lengths[1] = block_lengths[2] = 1;
-
-    MPI_Get_address(&indata, &addresses[0]);
-    MPI_Get_address(&(indata.a), &addresses[1]);
-    MPI_Get_address(&(indata.b), &addresses[2]);
-    MPI_Get_address(&(indata.n), &addresses[3]);
-
-    displacements[0] = addresses[1] - addresses[0];
-    displacements[1] = addresses[2] - addresses[0];
-    displacements[2] = addresses[3] - addresses[0];
-
-    blocks_number = 3;
-
-    MPI_Type_create_struct(blocks_number, block_lengths, displacements, typelist, &NEW_MESSAGE_TYPE); 
-
-    MPI_Type_commit(&NEW_MESSAGE_TYPE);
-
-    if (myrank == 0)
-    {
-        indata.a = 3.14159;
-        indata.b = 2.71828;
-        indata.n = 2002;
-
-        MPI_Send(&indata, 1, NEW_MESSAGE_TYPE, 1, tag, MPI_COMM_WORLD);
-        printf("Process %i send: %f %f %i\n",
-            myrank, indata.a, indata.b, indata.n);
-    }
-    else
-    {
-        MPI_Recv(&indata, 1, NEW_MESSAGE_TYPE, 0,
-            tag, MPI_COMM_WORLD, &status);
-        printf("Process %i received: %f %f %i, status %d\n",
-            myrank, indata.a, indata.b, indata.n, status.MPI_ERROR);
-    }
-
-    MPI_Finalize();
-    return 0;
-}
+﻿//#include <mpi.h>
+//#include <stdio.h>
+//
+//struct newtype
+//{
+//    float a;
+//    float b; 
+//    int n;
+//};
+//
+//int main(int argc, char* argv[])
+//{
+//    int myrank;
+//    MPI_Datatype NEW_MESSAGE_TYPE;
+//    int block_lengths[3];
+//    MPI_Aint displacements[3];
+//    MPI_Aint addresses[4];
+//    MPI_Datatype typelist[3];
+//    int blocks_number;
+//    struct newtype indata;
+//    int tag = 0;
+//    MPI_Status status;
+//
+//    MPI_Init(&argc, &argv);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+//
+//    typelist[0] = MPI_FLOAT;
+//    typelist[1] = MPI_FLOAT;
+//    typelist[2] = MPI_INT;
+//
+//    block_lengths[0] = block_lengths[1] = block_lengths[2] = 1;
+//
+//    MPI_Get_address(&indata, &addresses[0]);
+//    MPI_Get_address(&(indata.a), &addresses[1]);
+//    MPI_Get_address(&(indata.b), &addresses[2]);
+//    MPI_Get_address(&(indata.n), &addresses[3]);
+//
+//    displacements[0] = addresses[1] - addresses[0];
+//    displacements[1] = addresses[2] - addresses[0];
+//    displacements[2] = addresses[3] - addresses[0];
+//
+//    blocks_number = 3;
+//
+//    MPI_Type_create_struct(blocks_number, block_lengths, displacements, typelist, &NEW_MESSAGE_TYPE); 
+//
+//    MPI_Type_commit(&NEW_MESSAGE_TYPE);
+//
+//    if (myrank == 0)
+//    {
+//        indata.a = 3.14159;
+//        indata.b = 2.71828;
+//        indata.n = 2002;
+//
+//        MPI_Send(&indata, 1, NEW_MESSAGE_TYPE, 1, tag, MPI_COMM_WORLD);
+//        printf("Process %i send: %f %f %i\n",
+//            myrank, indata.a, indata.b, indata.n);
+//    }
+//    else
+//    {
+//        MPI_Recv(&indata, 1, NEW_MESSAGE_TYPE, 0,
+//            tag, MPI_COMM_WORLD, &status);
+//        printf("Process %i received: %f %f %i, status %d\n",
+//            myrank, indata.a, indata.b, indata.n, status.MPI_ERROR);
+//    }
+//
+//    MPI_Finalize();
+//    return 0;
+//}
---
+++ Task5.1.cpp
@@ -0,0 +1,37 @@
+﻿#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    MPI_Comm grid_comm;
+    int dims[2];
+    int periodic[2];
+    int reorder = 1, q = 5, ndims = 2, maxdims = 2;
+    int coordinates[2];
+    int my_grid_rank;
+    int coords[2];
+    MPI_Comm row_comm;
+
+    dims[0] = dims[1] = q;
+    periodic[0] = periodic[1] = 1;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_grid_rank); // Додано виклик для визначення рангу кожного процесу
+
+    MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periodic, reorder, &grid_comm);
+    MPI_Cart_coords(grid_comm, my_grid_rank, ndims, coordinates); // Додано виклик для отримання декартових координат
+
+    printf("Process rank %d has coordinates (%d, %d)\n", my_grid_rank, coordinates[0], coordinates[1]);
+
+    // Додано операції циклічного зміщення
+    int right_neighbor, left_neighbor;
+    MPI_Cart_shift(grid_comm, 0, 1, &left_neighbor, &right_neighbor); // Зміщення вгору
+    printf("Process rank %d, left neighbor: %d, right neighbor: %d\n", my_grid_rank, left_neighbor, right_neighbor);
+
+    // Додано операції лінійного зміщення
+    int down_neighbor, up_neighbor;
+    MPI_Cart_shift(grid_comm, 1, 1, &up_neighbor, &down_neighbor); // Зміщення вправо
+    printf("Process rank %d, up neighbor: %d, down neighbor: %d\n", my_grid_rank, up_neighbor, down_neighbor);
+
+    MPI_Finalize();
+    return 0;
+}
